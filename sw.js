4977a45f59e325fb69e252bcfca07e227384bf9b
MiaChmurciakova <mia.chmurciakova@gmail.com>
2026-09-03 18:37:33 +0200

    fix textu

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Stranka se bere vzdycky nejdriv ze site, aby se nova verze projevila hned;
    ze zasoby se servíruje jen tehdy, kdyz telefon nema signal.
    Nazev CACHE prepisuje build.py podle cisla verze v index.html. */
-const CACHE = "brepta-v14";
+const CACHE = "brepta-v15";
 const ASSETS = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/apple-touch-icon.png"
